612b3d3b962c5175a03a2f479986ee8af6eb682c
PinsaraPerera <1pawanpinsara@gmail.com>
2024-12-09 22:51:05 +0530

    added JWT token of supabase authentication

--- app/db/db.py
+++ app/db/db.py
@@ -1,8 +1,8 @@
 from supabase import create_client, Client
-from app.core.config import SUPABASE_URL, SUPABASE_KEY, SUPABASE_BUCKET
+from app.core.config import SUPABASE_URL, SUPABASE_KEY, SUPABASE_BUCKET, SUPABASE_JWT_SECRET
 
 
-if not all([SUPABASE_URL, SUPABASE_KEY, SUPABASE_BUCKET]):
+if not all([SUPABASE_URL, SUPABASE_KEY, SUPABASE_BUCKET, SUPABASE_JWT_SECRET]):
     raise EnvironmentError("One or more Supabase environment variables are missing")
 
 # Initialize the Supabase client
