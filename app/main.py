4226470e3b4567fe70e616f09882dbb5c19972bf
PinsaraPerera <1pawanpinsara@gmail.com>
2024-12-13 00:53:37 +0530

    access some ports

--- app/main.py
+++ app/main.py
@@ -11,6 +11,9 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:3003",
     "http://localhost:8000",
 ]
 
