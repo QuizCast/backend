1c5ceef2de0090e910bd58326cc331f4270e8697
PinsaraPerera <1pawanpinsara@gmail.com>
2024-12-12 10:31:41 +0530

    add crud for additional endpoint

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -4,6 +4,7 @@ from app.schemas import user_schema
 from app.core.config import SUPABASE_URL, SUPABASE_KEY
 from app.db.db import supabase
 from app.crud import user_crud
+from app.utils.autherization import get_current_user
 
 router = APIRouter(
     prefix="/user",
@@ -18,6 +19,10 @@ async def get_users():
 async def create_user(user: user_schema.UserCreate):
     return user_crud.create_user(user)
 
-@router.put("/update_user", response_model=user_schema.UserResponse)
+@router.put("/update_user", response_model=user_schema.UserResponse, dependencies=[Depends(get_current_user)])
 async def update_user(user: user_schema.UserUpdate):
     return user_crud.update_user(user)
+
+@router.get("/get_user/{email}", response_model=user_schema.UserResponse, dependencies=[Depends(get_current_user)])
+async def get_user_by_email(email: str):
+    return user_crud.get_user_by_email(email)
